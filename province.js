745b0c5d272e0654a00daa35b3801bce5415ec3c
wulixz <937731647@qq.com>
2020-10-02 15:35:29 +0800

    Create province.js

---
+++ province.js
@@ -0,0 +1,18 @@
+var provinceNumber = [
+    {name: '安徽省', value: 5},
+    {name: '甘肃省', value: 4},
+    {name: '贵州省', value: 1},
+    {name: '河北省', value: 3},
+    {name: '河南省', value: 3},
+    {name: '黑龙江省', value: 2},
+    {name: '湖南省', value: 1},   
+    {name: '江苏省', value: 17},
+    {name: '江西省', value: 1},
+    {name: '辽宁省', value: 1},
+    {name: '青海省', value: 1},
+    {name: '山东省', value: 1},
+    {name: '山西省', value: 2},
+    {name: '陕西省', value: 1},
+    {name: '四川省', value: 2},
+    {name: '新疆维吾尔自治区', value: 1}
+];
